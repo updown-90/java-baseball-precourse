a2dcf8773377fd1df817fc73748c22e999589b0a
updown-90 <updown_90@kakao.com>
2021-05-06 00:12:43 +0900

    refactor: 1차 공통 리뷰사항 반영 enum 추가

---
+++ src/main/java/model/BallStatus.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum BallStatus {
+	NOTHING, STRIKE, BALL;
+
+	public boolean isNotNothing() {
+		return this != NOTHING;
+	}
+
+	public boolean isStrike() {
+		return this == STRIKE;
+	}
+
+	public boolean isBall() {
+		return this == BALL;
+	}
+}

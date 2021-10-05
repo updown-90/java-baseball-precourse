f921f2c69246668a43ce3422424f0bb79077462e
updown-kurly <updown_90@kakao.com>
2021-10-05 23:35:49 +0900

    refactor: 프래그램 시작점 Application 으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import controller.BaseBallGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+		BaseBallGame baseBallGame = new BaseBallGame();
+		baseBallGame.gameStart();
     }
 }
